61095940e6068a211a87e0606d51c25efb375cc1
Alexx2378 <gaviolakarl.martin@gmail.com>
2026-04-20 01:31:34 +0800

    added the db, role, and send-email

---
+++ node-mysql-api/_helper/db.ts
@@ -0,0 +1,28 @@
+import config from '../config.json';
+import { createConnection } from 'mysql2/promise';
+import { Sequelize } from 'sequelize';
+import accountModel from '../accounts/account.model';
+import refresTokenModel from '../accounts/refreshtoken.model';
+
+const db: any = {};
+export default db;
+
+initialize();
+
+async function initialize() {
+    const { host, port, user, password, database } = config.database;
+    const connection = await createConnection({ host, port, user, password });
+
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+    await connection.end();
+
+    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+
+    db.Account = accountModel(sequelize);
+    db.RefreshToken = refresTokenModel(sequelize);
+
+    db.Account.hasMany(db.RefreshToken, { onDelete: 'CASCADE' });
+    db.RefreshToken.belongsTo(db.Account);
+
+    await sequelize.sync();
+}
